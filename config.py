0ab7ddd3c735968d2128b128a9afb98d7b640c6c
emayberry74 <ethan.mayberry@okstate.edu>
2018-10-02 12:00:46 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
-NICK = "cubetron"
-PASS = "oauth:bs5zwe4flasavb7jd2tth2zyn03lam"
-CHAN = "jimondaspot"
+NICK = ""
+PASS = ""
+CHAN = ""
 RATE = (20/30) # messages per second
 
 channels = []
